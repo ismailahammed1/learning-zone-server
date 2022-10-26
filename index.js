61ce97eac4ec14f69d56454dc17af42689a34ddf
Md Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2022-10-26 11:03:59 +0600

    create fake data

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const language = require("./data/languages.json");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("look mama!");
+});
+
+app.get("/language", (req, res) => {
+  res.send(language);
+});
+app.get("/language/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("looking for id for", id);
+  const languages = language.find((ph) => ph.id === id);
+  res.send(languages);
+});
+
+app.listen(port, () => {
+  console.log(`iphone server`);
+});
